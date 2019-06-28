1a038420f60a8172fc9b5c63571a6c295b278cf9
Mingzi Xu <xu000198@algonquinlive.com>
2019-06-28 11:38:35 -0400

    Added code for Week 6, emit and slots

--- src/views/Todolist.vue
+++ src/views/Todolist.vue
@@ -20,19 +20,21 @@
 import Todo from "@/components/Todo.vue";
 //0621
 import TodoForm from "@/components/TodoForm.vue";
-
+import axios from "axios"
 export default {
-  data() {
-    return {
-      todoList: ["walk the dag", "go for a ride"]
-    };
-  },
   components: {
     //tell we want to use and display Todo tag
     Todo,
     //0621
     TodoForm
   },
+  data() {
+    return {
+      todoList: []
+      //---default todolist statement, can be todoList: ["my first to do."]
+    };
+  },
+  
   //0621-add methods
   methods: {
     appDeleteTodo(index) {
@@ -40,9 +42,52 @@ export default {
     },
     addTodo(todo) {
       this.todoList.push(todo);
+      axios
+        .put("https://mingzix-vue-and-axios.firebaseio.com/data.json", this.todoList)
+        .then(response => {
+          console.log(response);
+          console.log("Your data was saved status:" + response.status);
+        })
+        .catch(error=>{
+          console.log(error);
+        });
     }
-  }
-};
+  },
+  /*beforeCreated(){
+    console.log("before app is created");
+  },
+  created() {
+    console.log("App is created");
+  },
+  beforeMount(){
+    console.log("before app is mounted");
+  },
+  mounted(){
+    console.log("App is mounted");
+  },
+  */
+ //make sure outside methods function
+ created() {
+   axios
+    .get("https://mingzix-vue-and-axios.firebaseio.com/data.json")
+    //if it is successful
+    //js function: function (response) { }
+    .then(
+      response => {
+        // console.log(response);
+          console.log(response.data);//----get array of data in console
+          if(response.data){
+          this.todoList = response.data;
+          }
+          /*-----if(response.data)
+          this.todoList = response.data;
+          */
+        })
+    .catch(error => {
+      console.log("There was an error in getting data: " + error.response);
+    });
+ }
+};//if axios request is successful, then()will run, if not catch()will run
 </script>
 
 <style>
